c3afd87070081d3f7b72ae5d46ebadadd9b77288
Arunmudiraaj <arunmudiraj18@gmail.com>
2022-12-29 20:41:25 +0530

    third

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ let submit = document.querySelector('#submit')
 submit.addEventListener('click',submitted)
 
 function submitted(){
-    console.log(username.value,useremail.value)
-    localStorage.setItem(username.value,useremail.value)
+    var obj = {
+        name : username.value,
+        email : useremail.value,
+    }
+    var strFormat = JSON.stringify(obj)
+    localStorage.setItem('user',strFormat)
+    console.log(JSON.parse(localStorage.getItem('user')))
 }
