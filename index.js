7b5938b5e674100cf61bd711acd086198f86a5f1
Arunmudiraaj <arunmudiraj18@gmail.com>
2022-12-30 23:31:26 +0530

    final

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-let username = document.querySelector('#name')
-let useremail = document.querySelector('#email')
-let submit = document.querySelector('#submit')
+// let username = document.querySelector('#name')
+// let useremail = document.querySelector('#email')
+// let submit = document.querySelector('#submit')
+// let users = document.querySelector('#users')
+// let allusers = document.querySelectorAll('.delete')
+// submit.addEventListener('click',submitted)
 
-submit.addEventListener('click',submitted)
-
-function submitted(){
-    var obj = {
-        name : username.value,
-        email : useremail.value,
-    }
-    var strFormat = JSON.stringify(obj)
-    localStorage.setItem(useremail.value,strFormat)
-    console.log(JSON.parse(localStorage.getItem('user')))
-}
+// function submitted(){
+//     var obj = {
+//         name : username.value,
+//         email : useremail.value,
+//     }
+//     var strFormat = JSON.stringify(obj)
+//     localStorage.setItem(useremail.value,strFormat)
+//     console.log(JSON.parse(localStorage.getItem('user')))
+//     users.innerHTML =  `${users.innerHTML} <li class="item"> ${username.value} ${useremail.value} <button onclick="deleted()" class="bg-dark delete">Delete</button> </li>`
+    
+// }
+// function deleted(e){
+//     console.log(e.target)
+// }
